47610c8f8e44750814cebce0205084fae6285e05
Jeremy Chong <h09083.cys@gmail.com>
2021-03-29 11:53:28 +0800

    Checkpoint before start programming for the day

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+//! >>>>>>>>>> IMPORT START <<<<<<<<<<
+//! >>>>>>>>>> IMPORT START <<<<<<<<<<
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
+// Import Fonts
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+// Import Theme
 import { theme } from "./src/infrastructure/theme";
+// Import Screens
 import { TuitionsScreen } from "./src/features/tuitions/screens/tuitions.screen";
 import { ProfileScreen } from "./src/features/profile/screens/profile.screen";
+//! >>>>>>>>>> IMPORT END <<<<<<<<<<
+//! >>>>>>>>>> IMPORT END <<<<<<<<<<
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
