0e96eb430248e9d15d9c3f2b7924b7363f1b966a
Jeremy Chong <h09083.cys@gmail.com>
2021-03-29 12:22:11 +0800

    Created Bottom Navigation Tab

--- App.js
+++ App.js
@@ -3,6 +3,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+// Import Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // Import Fonts
 import {
@@ -19,6 +22,38 @@ import { ProfileScreen } from "./src/features/profile/screens/profile.screen";
 //! >>>>>>>>>> IMPORT END <<<<<<<<<<
 //! >>>>>>>>>> IMPORT END <<<<<<<<<<
 
+// Navigation Element
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Tuitions" component={TuitionsScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+/*
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -42,3 +77,4 @@ export default function App() {
     </>
   );
 }
+*/
